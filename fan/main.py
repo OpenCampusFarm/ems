0f29d487c67d0deaf05bcf6637222f7da26f3f2c
Joeky <joeky5888@gmail.com>
2026-04-20 11:06:41 -0400

    fanmode auto
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- fan/main.py
+++ fan/main.py
@@ -43,7 +43,7 @@ class FanMode(enum.Enum):
 # --- Fan / Relay config ---
 RELAY_PIN = 23
 POLL_INTERVAL = 30  # seconds between checks
-FAN_MODE = FanMode.FORCE_OFF
+FAN_MODE = FanMode.AUTO
 HYSTERESIS = 2.0  # °F deadband
 # --- CoolBot config ---
 BLYNK_URL = "wss://cbws.storeitcold.com/websocket"
