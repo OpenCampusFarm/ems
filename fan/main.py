ab298b4a23313f03597cc2031ee254a5cb8044c1
Rachel Mecca <remecca@umich.edu>
2026-09-23 13:36:51 -0400

    turn fan off

--- fan/main.py
+++ fan/main.py
@@ -39,7 +39,7 @@ class FanMode(enum.Enum):
 # --- Fan / Relay config ---
 RELAY_PIN = 23
 POLL_INTERVAL = 30  # seconds between checks
-FAN_MODE = FanMode.FORCE_ON
+FAN_MODE = FanMode.FORCE_OFF
 HYSTERESIS = 2.0  # °F deadband
 
 # The coordinates for the UM Campus Farm
